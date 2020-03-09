584476f98215af15f41bab10bdad5125d4806b6d
Wagner <wagner.signoretti@acad.pucrs.br>
2020-03-09 16:27:00 -0300

    First commit

---
+++ confenvironment/src/main/java/wagner_repository/confenvironment/App.java
@@ -0,0 +1,13 @@
+package wagner_repository.confenvironment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
